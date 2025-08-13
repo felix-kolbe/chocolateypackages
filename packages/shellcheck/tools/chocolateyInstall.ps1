4f2f2175bf05628c19c1ab6e86a9bee588693488
Felix Kolbe <felix.kolbe@gmail.com>
2025-08-13 19:03:44 +0200

    Update shellcheck to 0.11.0

--- packages/shellcheck/tools/chocolateyInstall.ps1
+++ packages/shellcheck/tools/chocolateyInstall.ps1
@@ -3,7 +3,7 @@ $content = Join-Path (Split-Path $tools) 'content'
 
 Install-ChocolateyZipPackage `
     -PackageName 'shellcheck' `
-    -Url 'https://github.com/koalaman/shellcheck/releases/download/v0.8.0/shellcheck-v0.8.0.zip' `
-    -Checksum 'cc5208d9f8799d792122204196fafb700801b4bae9bbb0a8f8a999f0a13cca1bfad440b96a3746740d85da55901e1d652592490bc196afc8bc0ebd0ae20b9aa1' `
+    -Url 'https://github.com/koalaman/shellcheck/releases/download/v0.11.0/shellcheck-v0.11.0.zip' `
+    -Checksum '910a27486747ff78dc8a036db7460ef4f3278658a87fc19b79de2fde5deca274be651b03e171ea1bafaaae6560d8e3d65e7e119fe7889bc83a60aef0dca8623f' `
     -ChecksumType 'SHA512' `
     -UnzipLocation $content
